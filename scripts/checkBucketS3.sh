4f4c93829d4b66939fca3ee7b89e643896804734
milunadev <misusuyatec@gmail.com>
2024-01-24 22:23:40 -0500

    Agregando la carpeta de scripts

---
+++ scripts/checkBucketS3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUCKET_NAME=$1
+REGION=$2
+
+if aws s3 ls "s3://$BUCKET_NAME" 2>&1 | grep -q 'NoSuchBucket'
+then
+  echo "El bucket no existe. Creando bucket: $BUCKET_NAME"
+  aws s3 mb "s3://$BUCKET_NAME" --region "$REGION"
+else
+  echo "El bucket ya existe: $BUCKET_NAME"
+fi
